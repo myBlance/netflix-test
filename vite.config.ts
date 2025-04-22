40e41a74b371ea827aeb6cb174da6f78aad5f069
bblanc <tranlongh2003@gmail.com>
2025-04-22 13:43:49 +0700

    cap nhat

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: [
+      'netflix-test-flame.vercel.app', // ✅ Không có http:// ngrok http 5173
+      'localhost',
+    ],
     proxy: {
       '/api': {
         target: 'http://localhost:4000',
